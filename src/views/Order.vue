76deac9b90aa041702707c0aee790d3097403607
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-12-27 08:01:25 +0300

    edit: added orders adding and editing
    Took 1 hour 40 minutes

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,11 +1,85 @@
 <template>
+  <div class="container">
+    <h1 class="display-3">Order details</h1>
+    <form @submit="handleSubmit">
+      <div class="form-row">
+        <label for="id">ID</label>
+        <input id="id" v-model="id" class="form-control" disabled type="text">
+      </div>
 
+      <div class="form-row">
+        <label for="login">Table</label>
+        <input id="login" v-model="tableID" class="form-control" type="number">
+      </div>
+      <div class="form-check">
+        <input id="available" v-model="ready" class="form-check-input" type="checkbox">
+        <label for="available">Ready</label>
+      </div>
+      <div class="form-row"><label>Waiter
+        <select v-model="waiterID" class="custom-select">
+          <option selected value="0">Undefined</option>
+          <option v-for="waiter in waiters" :value="waiter.id">{{ waiter.name }}</option>
+        </select>
+      </label></div>
+      <button class="btn btn-success" type="submit">Save</button>
+    </form>
+  </div>
 </template>
 
 <script>
+import DataService from "@/services/DataService";
+
 export default {
-  name: "Order"
-}
+  name: "Order",
+  data() {
+    return {
+      waiterID: 0,
+      tableID: 0,
+      ready: false,
+      waiters: null,
+      serverUrl: "http://localhost:8080/order/"
+    };
+  },
+  computed: {
+    id() {
+      return parseInt(this.$route.params.id);
+    }
+  },
+  methods: {
+    refreshDetails() {
+      DataService.get(this.serverUrl, this.id).then(res => {
+        this.waiterID = res.data.waiterID;
+        this.tableID = res.data.tableID;
+        this.ready = res.data.ready;
+      });
+    },
+    handleSubmit: function (e) {
+      e.preventDefault();
+      const item = {
+        waiterID: this.waiterID,
+        tableID: this.tableID,
+        ready: this.ready
+      };
+      if (this.id !== 0) {
+        DataService.update(this.serverUrl, this.id, item).then(() => {
+          this.$router.push("/orders")
+        })
+      } else {
+        DataService.create(this.serverUrl, item).then(() => {
+          this.$router.push("/orders")
+        })
+      }
+    }
+  },
+  created() {
+    if (this.id !== 0) {
+      this.refreshDetails();
+    }
+    DataService.getAll("http://localhost:8080/user").then(res => {
+      this.waiters = res.data;
+    })
+  }
+};
 </script>
 
 <style scoped>
--- src/views/OrderList.vue
+++ src/views/OrderList.vue
@@ -1,12 +1,115 @@
 <template>
   <div class="container">
     <h2 class="display-3">Orders</h2>
+    <div class="btn btn-success mb-3" v-on:click="add()">Add</div>
+    <div v-if="message" id="deleteMessage" class="alert alert-danger alert-dismissible fade show" role="alert">
+      <h4>Warning!</h4>
+      {{ message }}
+      <button class="btn btn-outline-danger" data-dismiss="alert" type="button" v-on:click="undo()">Cancel
+      </button>
+
+      <button aria-label="Close" class="close" data-dismiss="alert" type="button" v-on:click="deleteMessage()">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+
+    <div class="card-deck">
+      <div v-for="order in orders" v-if="!order.ready" class="card bg-light mb-3">
+        <div class="card-header">{{ order.id % 100 }}</div>
+        <div class="card-body">
+          <h4 class="card-title">
+            {{ order.waiterID }}
+          </h4>
+          <h6 class="card-subtitle">Table {{ order.table }}</h6>
+          <div class="card-text">
+            <ul>
+              <li v-for="meal in order.items">{{ meal }}</li>
+            </ul>
+          </div>
+          <div class="btn btn-outline-dark" v-on:click="update(order.id)">Edit</div>
+        </div>
+      </div>
+      <div v-for="order in orders" v-if="order.ready" class="card text-white bg-dark mb-3">
+        <div class="card-header">{{ order.id % 100 }}</div>
+        <div class="card-body">
+          <h4 class="card-title">
+            {{ order.waiterID }}
+          </h4>
+          <h6 class="card-subtitle">Table {{ order.table }}</h6>
+          <div class="card-text">
+            <ul>
+              <li v-for="meal in order.items">{{ meal }}</li>
+            </ul>
+          </div>
+          <div class="btn btn-outline-light mr-2" v-on:click="update(order.id)">Edit</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import DataService from "@/services/DataService";
+
 export default {
-  name: "OrderList"
+  name: "OrderList",
+  data() {
+    return {
+      orders: [],
+      message: null,
+      last: null,
+      serverUrl: "http://localhost:8080/order/"
+    }
+  },
+  methods: {
+    refresh() {
+      DataService.getAll(this.serverUrl).then(response => {
+        this.orders = response.data;
+        // this.orders = [{
+        //   id: 2,
+        //   waiterID: 'Sabina',
+        //   table: 2,
+        //   items: ["adfasdf", "adfasd", "asdfsd"],
+        //   ready: true
+        // }, {
+        //   id: 3,
+        //   waiterID: 'Siroj',
+        //   table: 5,
+        //   items: ["adfasdf", "adfasd", "asdfsd"],
+        //   ready: false
+        // }]
+      })
+    },
+    deleteItem(id) {
+      DataService.delete(this.serverUrl, id).then(response => {
+        this.message = 'Deleted ' + response.data.mealName + ' successful';
+        this.last = {
+          id: response.data.id,
+          mealAvailable: response.data.mealAvailable,
+          mealCost: response.data.mealCost,
+          mealName: response.data.mealName
+        };
+        this.refresh();
+      })
+    },
+    update(id) {
+      this.$router.push('/orders/' + id)
+    },
+    add() {
+      this.$router.push(`/orders/0`)
+    },
+    async undo() {
+      await DataService.create(this.serverUrl, this.last);
+      this.refresh();
+      this.message = null;
+    },
+    deleteMessage() {
+      this.message = null;
+    }
+  },
+  created() {
+    this.refresh();
+  }
 }
 </script>
 
